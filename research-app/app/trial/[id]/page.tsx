a2dad5058930f2f240a87fb4f2beb84fece70644
kakizawaayato <chibairodori25@gmail.com>
2023-05-10 23:19:52 +0900

    feat: ルーティング

---
+++ research-app/app/trial/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Hoge } from '@/app/components/hoge';
+
+const TrialIdPage: React.FC = () => {
+  return (
+    <>
+      <Hoge />
+    </>
+  );
+};
+
+export default TrialIdPage;
---
+++ research-app/app/trial/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const TrialPage: NextPage = () => {
+  return <>TrialPageです。</>;
+};
+
+export default TrialPage;
